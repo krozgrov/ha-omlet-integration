81e2209d5282b451bb0df722d3cdd8ba6c8a1738
Matthew Cosgrove <krozgrov@hotmail.com>
2024-12-29 10:00:21 -0500

    Update time interval

--- custom_components/omlet/coordinator.py
+++ custom_components/omlet/coordinator.py
@@ -5,16 +5,17 @@ from .api_client import OmletApiClient
 
 _LOGGER = logging.getLogger(__name__)
 
+
 class OmletDataCoordinator(DataUpdateCoordinator):
     """Coordinator for fetching and structuring data from the Omlet API."""
 
-    def __init__(self, hass, api_key, update_interval):
+    def __init__(self, hass, api_key, update_interval_seconds):
         """Initialize the coordinator."""
         super().__init__(
             hass,
             _LOGGER,
             name="OmletDataCoordinator",
-            update_interval=timedelta(seconds=update_interval),
+            update_interval=timedelta(seconds=update_interval_seconds),
         )
         self.api_key = api_key
         self.api_client = OmletApiClient(api_key)
@@ -44,24 +45,44 @@ class OmletDataCoordinator(DataUpdateCoordinator):
                     "bleOnly": device.get("bleOnly", 0),
                     "state": {
                         "general": {
-                            "firmwareVersionCurrent": device["state"]["general"].get("firmwareVersionCurrent"),
-                            "firmwareLastCheck": device["state"]["general"].get("firmwareLastCheck"),
-                            "batteryLevel": device["state"]["general"].get("batteryLevel"),
-                            "powerSource": device["state"]["general"].get("powerSource"),
-                            "displayLine1": device["state"]["general"].get("displayLine1"),
-                            "displayLine2": device["state"]["general"].get("displayLine2"),
-                            "firmwareVersionPrevious": device["state"]["general"].get("firmwareVersionPrevious"),
+                            "firmwareVersionCurrent": device["state"]["general"].get(
+                                "firmwareVersionCurrent"
+                            ),
+                            "firmwareLastCheck": device["state"]["general"].get(
+                                "firmwareLastCheck"
+                            ),
+                            "batteryLevel": device["state"]["general"].get(
+                                "batteryLevel"
+                            ),
+                            "powerSource": device["state"]["general"].get(
+                                "powerSource"
+                            ),
+                            "displayLine1": device["state"]["general"].get(
+                                "displayLine1"
+                            ),
+                            "displayLine2": device["state"]["general"].get(
+                                "displayLine2"
+                            ),
+                            "firmwareVersionPrevious": device["state"]["general"].get(
+                                "firmwareVersionPrevious"
+                            ),
                             "uptime": device["state"]["general"].get("uptime"),
                         },
                         "connectivity": {
-                            "wifiStrength": device["state"]["connectivity"].get("wifiStrength"),
+                            "wifiStrength": device["state"]["connectivity"].get(
+                                "wifiStrength"
+                            ),
                             "ssid": device["state"]["connectivity"].get("ssid"),
-                            "connected": device["state"]["connectivity"].get("connected"),
+                            "connected": device["state"]["connectivity"].get(
+                                "connected"
+                            ),
                         },
                         "door": {
                             "state": device["state"]["door"].get("state"),
                             "lastOpenTime": device["state"]["door"].get("lastOpenTime"),
-                            "lastCloseTime": device["state"]["door"].get("lastCloseTime"),
+                            "lastCloseTime": device["state"]["door"].get(
+                                "lastCloseTime"
+                            ),
                             "fault": device["state"]["door"].get("fault"),
                             "lightLevel": device["state"]["door"].get("lightLevel"),
                         },
@@ -72,36 +93,76 @@ class OmletDataCoordinator(DataUpdateCoordinator):
                     "configuration": {
                         "light": {
                             "mode": device["configuration"]["light"].get("mode"),
-                            "minutesBeforeClose": device["configuration"]["light"].get("minutesBeforeClose"),
-                            "maxOnTime": device["configuration"]["light"].get("maxOnTime"),
-                            "equipped": device["configuration"]["light"].get("equipped"),
+                            "minutesBeforeClose": device["configuration"]["light"].get(
+                                "minutesBeforeClose"
+                            ),
+                            "maxOnTime": device["configuration"]["light"].get(
+                                "maxOnTime"
+                            ),
+                            "equipped": device["configuration"]["light"].get(
+                                "equipped"
+                            ),
                         },
                         "door": {
                             "openMode": device["configuration"]["door"].get("openMode"),
-                            "openDelay": device["configuration"]["door"].get("openDelay"),
+                            "openDelay": device["configuration"]["door"].get(
+                                "openDelay"
+                            ),
                             "openTime": device["configuration"]["door"].get("openTime"),
-                            "closeMode": device["configuration"]["door"].get("closeMode"),
-                            "closeDelay": device["configuration"]["door"].get("closeDelay"),
-                            "closeLightLevel": device["configuration"]["door"].get("closeLightLevel"),
-                            "closeTime": device["configuration"]["door"].get("closeTime"),
-                            "openLightLevel": device["configuration"]["door"].get("openLightLevel"),
+                            "closeMode": device["configuration"]["door"].get(
+                                "closeMode"
+                            ),
+                            "closeDelay": device["configuration"]["door"].get(
+                                "closeDelay"
+                            ),
+                            "closeLightLevel": device["configuration"]["door"].get(
+                                "closeLightLevel"
+                            ),
+                            "closeTime": device["configuration"]["door"].get(
+                                "closeTime"
+                            ),
+                            "openLightLevel": device["configuration"]["door"].get(
+                                "openLightLevel"
+                            ),
                             "doorType": device["configuration"]["door"].get("doorType"),
                             "colour": device["configuration"]["door"].get("colour"),
                         },
                         "connectivity": {
-                            "wifiState": device["configuration"]["connectivity"].get("wifiState"),
+                            "wifiState": device["configuration"]["connectivity"].get(
+                                "wifiState"
+                            ),
                         },
                         "general": {
-                            "datetime": device["configuration"]["general"].get("datetime"),
-                            "timezone": device["configuration"]["general"].get("timezone"),
-                            "updateFrequency": device["configuration"]["general"].get("updateFrequency"),
-                            "language": device["configuration"]["general"].get("language"),
-                            "stayAliveTime": device["configuration"]["general"].get("stayAliveTime"),
-                            "statusUpdatePeriod": device["configuration"]["general"].get("statusUpdatePeriod"),
-                            "pollFreq": device["configuration"]["general"].get("pollFreq"),
-                            "overnightSleepEnable": device["configuration"]["general"].get("overnightSleepEnable"),
-                            "overnightSleepStart": device["configuration"]["general"].get("overnightSleepStart"),
-                            "overnightSleepEnd": device["configuration"]["general"].get("overnightSleepEnd"),
+                            "datetime": device["configuration"]["general"].get(
+                                "datetime"
+                            ),
+                            "timezone": device["configuration"]["general"].get(
+                                "timezone"
+                            ),
+                            "updateFrequency": device["configuration"]["general"].get(
+                                "updateFrequency"
+                            ),
+                            "language": device["configuration"]["general"].get(
+                                "language"
+                            ),
+                            "stayAliveTime": device["configuration"]["general"].get(
+                                "stayAliveTime"
+                            ),
+                            "statusUpdatePeriod": device["configuration"][
+                                "general"
+                            ].get("statusUpdatePeriod"),
+                            "pollFreq": device["configuration"]["general"].get(
+                                "pollFreq"
+                            ),
+                            "overnightSleepEnable": device["configuration"][
+                                "general"
+                            ].get("overnightSleepEnable"),
+                            "overnightSleepStart": device["configuration"][
+                                "general"
+                            ].get("overnightSleepStart"),
+                            "overnightSleepEnd": device["configuration"]["general"].get(
+                                "overnightSleepEnd"
+                            ),
                         },
                     },
                     "deviceType": device.get("deviceType"),
